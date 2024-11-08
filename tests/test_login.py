06828b0eae4f16a3143fbc5d5ea562f7e84e7b2f
Rizqi Raflian Ukasah <raflianrizqi@gmail.com>
2024-11-08 08:39:33 +0800

    update_code_#4
    - refactore code to use globalVariable

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,6 +1,7 @@
-import pytest
 from selenium.webdriver.common.by import By
 from selenium_helpers import wait_for_element,allure,os
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 @allure.suite("Login Tests Suite")
@@ -8,16 +9,16 @@ class TestLogin:
     
     @allure.title("Successful Login Test")
     @allure.severity(allure.severity_level.BLOCKER)
-    def test_login_success(self, driver,adminUsername,adminPassword,capture_screenshot):
+    def test_login_success(self, driver,capture_screenshot,globalVariable):
         with allure.step("Access orangeHRMDemo website"):
             driver.get("https://opensource-demo.orangehrmlive.com")
         with allure.step("Input admin username {adminUsername} with password {adminPassword}"):
             # Locate and fill in the username
             username_field = driver.find_element(By.NAME, "username")
-            username_field.send_keys(adminUsername)
+            username_field.send_keys(globalVariable["adminUsername"])
             # Locate and fill in the password
             password_field = driver.find_element(By.NAME, "password")
-            password_field.send_keys(adminPassword)
+            password_field.send_keys(globalVariable["adminPassword"])
         with allure.step("click login button"):    
             # Locate and click the login button
             login_button = driver.find_element(By.XPATH, "//button[@type='submit']")
@@ -32,25 +33,27 @@ class TestLogin:
                 capture_screenshot("login_failed")
                 raise
 
-    @allure.title("Failed Login Test")
-    @allure.severity(allure.severity_level.CRITICAL)
-    def test_login_failed(self, driver,admin_wrongUsername,universalPassword,capture_screenshot):
+    @allure.title("Test Error Validation")
+    @allure.severity(allure.severity_level.NORMAL)
+    def test_login_failed(self,driver,globalVariable,capture_screenshot):
         with allure.step("Access orangeHRMDemo website"):
             driver.get("https://opensource-demo.orangehrmlive.com")
-            # Set an implicit wait
-            driver.implicitly_wait(10)  # Wait up to 10 seconds for elements to appear
-        with allure.step("Input admin username {admin_wrongUsername} with password {universalPassword}"):
-            # Locate and fill in the username
-            username_field = driver.find_element(By.NAME, "username")
-            username_field.send_keys(admin_wrongUsername)
-            # Locate and fill in the password
-            password_field = driver.find_element(By.NAME, "password")
-            password_field.send_keys(universalPassword)
+        
+        # Check the validation if the credential is invalid
+        with allure.step("Check if the credential is invalid"):
+            with allure.step("input invalid username"):
+                # Locate and fill in the username
+                username_field = driver.find_element(By.NAME, "username")
+                username_field.send_keys(globalVariable["dummyUsername"])
+            with allure.step("input invalid password"):
+                # Locate and fill in the password
+                password_field = driver.find_element(By.NAME, "password")
+                password_field.send_keys(globalVariable["adminPassword"])
         with allure.step("click login button"):
             # Locate and click the login button
             login_button = driver.find_element(By.XPATH, "//button[@type='submit']")
             login_button.click()
-        with allure.step("Verify the error validation"):
+        with allure.step("Verify the error handling for invalid credential is correct"):
             error_validation = wait_for_element(driver, By.XPATH, "//div[@role='alert']//div[1]", timeout=10)
             try:
                 assert error_validation.is_displayed
@@ -59,4 +62,23 @@ class TestLogin:
                 capture_screenshot("error message is not displayed")
                 raise 
 
-
+        # Check the validation if the mandatory field is not populated
+        username_field = driver.find_element(By.NAME, "username")
+        username_field.clear()
+        password_field = driver.find_element(By.NAME, "password")
+        password_field.clear()
+        with allure.step("Check the mandatory field validation"):
+            with allure.step("click login button with blank value of username and password"):
+                # Locate and click the login button
+                login_button = driver.find_element(By.XPATH, "//button[@type='submit']")
+                login_button.click()
+            with allure.step("Verify the error handling for mandatory field"):
+                error_validation_username = wait_for_element(driver, By.XPATH, "(//span[@class='oxd-text oxd-text--span oxd-input-field-error-message oxd-input-group__message'][normalize-space()='Required'])[1]", timeout=10)
+                error_validation_password = wait_for_element(driver, By.XPATH, "(//span[@class='oxd-text oxd-text--span oxd-input-field-error-message oxd-input-group__message'][normalize-space()='Required'])[2]", timeout=10)
+                try:
+                    assert error_validation_username.is_displayed
+                    assert error_validation_password.is_displayed
+                    capture_screenshot("error message is displayed")
+                except AssertionError:
+                    capture_screenshot("error message is not displayed")
+                    raise 
